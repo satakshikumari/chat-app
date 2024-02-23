7f9b5a045143e23da419ee86385f508e016d8e6a
Satakshi Kumari <sakshiojha17.so@gmail.com>
2024-02-23 12:28:43 +0530

    add health check route

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get("/health", (req, res)=>{
+  return res.status(200).json({
+    message: "Server is healthy!"
+  })
+})
+
 const users = {}; 
 
 io.on('connection', (socket) => {
